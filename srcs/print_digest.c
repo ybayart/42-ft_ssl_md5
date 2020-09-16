31c48297cca8c62f766216cf7c18315e25098f91
hexanyn <hexanyn@gmail.com>
2020-09-16 18:16:48 +0200

    fix display

--- srcs/print_digest.c
+++ srcs/print_digest.c
@@ -6,7 +6,7 @@
 /*   By: hexa <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 19:24:47 by hexa              #+#    #+#             */
-/*   Updated: 2020/09/16 17:39:06 by hexa             ###   ########.fr       */
+/*   Updated: 2020/09/16 18:16:18 by hexa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ static void	infos(t_digest digest, t_parsing parsing, char state)
 		if (digest.std == 1)
 			write(1, "|> ", 4);
 		else if (digest.string == 1)
-			ft_puttab(1, 4, parsing.algo, "[\"", digest.file, "\"]= ");
+			ft_puttab(1, 4, parsing.algo, " [\"", digest.file, "\"] = ");
 		else
-			ft_puttab(1, 4, parsing.algo, "(", digest.file, ")= ");
+			ft_puttab(1, 4, parsing.algo, " (", digest.file, ") = ");
 	}
 	else if (state == 1 && parsing.reverse == 1 && parsing.quiet == 0)
 	{
@@ -42,8 +42,7 @@ static void	print_digest(t_digest digest, t_parsing parsing)
 	i = 0;
 	if (parsing.print == 1 && digest.std == 1 && digest.text != NULL)
 	{
-		if (parsing.quiet == 0)
-			write(1, digest.text, digest.len);
+		write(1, digest.text, digest.len);
 		write(1, "\n", 1);
 		free(digest.text);
 	}
