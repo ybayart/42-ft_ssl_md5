3f0deed2849630bc6c78cc5f8685b404365657ea
hexanyn <hexanyn@gmail.com>
2020-09-26 03:08:29 +0200

    fix padding == limit

--- srcs/padding.c
+++ srcs/padding.c
@@ -6,7 +6,7 @@
 /*   By: hexa <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 10:53:21 by hexa              #+#    #+#             */
-/*   Updated: 2020/09/26 03:06:11 by hexa             ###   ########.fr       */
+/*   Updated: 2020/09/26 03:07:59 by hexa             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void		padding(void *buf, size_t src, size_t size, void *(*fct)())
 	dst = (((dst % size > (size * 0.875) ?
 		dst + size : dst) / size) + 1) * size;
 	*((unsigned char*)buf + (src % size)) = (1 << 7);
-	if (src % size < (size * 0.875) - 1)
+	if (src % size < (size * 0.875))
 	{
 		if (size == 128)
 			padding_size_128(buf, src, size, fct);
